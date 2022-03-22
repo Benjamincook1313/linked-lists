e3b045ae6ad82b19a9de7a89c551ae3c2471eca5
Benjamin <benjamincook1313@gmail.com>
2022-03-22 09:29:32 -0600

    first commit

---
+++ values.js
@@ -0,0 +1,43 @@
+
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+};
+
+const a = new Node('A')
+const b = new Node('B')
+const c = new Node('C')
+const d = new Node('D')
+
+a.next = b
+b.next = c
+c.next = d
+
+// const listValues = (head) => {
+//   const values = []
+//   let current = head
+//   while(current != null) {
+//     values.push(current.val)
+//     current = current.next
+//   }
+//   console.log(values)
+//   return values
+// };
+
+// RECURSIVE
+const listValues = (head) => {
+  const values = []
+  fillValues(head, values)
+  console.log(values)
+  return values
+};
+
+const fillValues = (head, values) => {
+  if(head === null) return 
+  values.push(head.val)
+  fillValues(head.next, values)
+};
+
+listValues(a)
